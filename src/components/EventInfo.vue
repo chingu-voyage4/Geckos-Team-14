ca6e93a2a9addf4a55c05496a4ae3a1d591f85c6
Rinat <rinatkins@gmail.com>
2018-03-15 22:11:14 +0300

    [#5] Event object updated

--- src/components/EventInfo.vue
+++ src/components/EventInfo.vue
@@ -1,8 +1,17 @@
 <template>
   <div class="event-info">
-      <h1>{{events[this.$route.params.id].name}}</h1>
-      <div>{{events[this.$route.params.id].img}}</div>
-      <h3>{{ events[this.$route.params.id].description }}</h3>
+      <h1>Event name: {{ event.name }}</h1>
+      <img :src="event.img" class="ev-img" :alt="event.imgAlt">
+      <h3>Description: {{ event.description }}</h3>
+      <h3>Date: {{ event.date }}</h3>
+      <h3>City: {{ event.city }}</h3>
+      <h3>Address: {{ event.address }}</h3>
+      <h3>Venue name: {{ event.venueName }}</h3>
+      <h4>Time: {{ event.beginTime }} <span v-if="event.endTime!==''">-</span> {{ event.endTime }}</h4>
+      <h4>Company name: {{ event.company }}</h4>
+      <h4>Price: {{ event.price }}</h4>
+      <h4>Contact: {{ event.contactPhone }}</h4>
+      <h4>map coords: {{event.mapLongitude}}, {{event.mapLatitude}}</h4>
       <router-link to="/" tag = "button">Back</router-link>
   </div>
 </template>
@@ -10,12 +19,16 @@
 <script>
     export default {
         props: ['events'],
+        data(){
+          return {
+            event: this.events[this.$route.params.id]
+          }
+        }
     }
 </script>
 
 <style scoped>
     .event-info {
-        height: 400px;
         background-color: lightGreen;
         text-align: center;
     }
