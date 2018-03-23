9ff9528806149ee769900189f24652cf1722819c
Rinat <rinatkins@gmail.com>
2018-03-23 19:42:50 +0300

    [#6] added login/signup

---
+++ src/components/Signup.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div class="signup">
+      <input type="text" placeholder = "Email">
+      <br>
+      <input type="text" placeholder = "Password">
+      <br>
+      <input type="text" placeholder = "Confirm password">
+      <br>
+      <router-link to="/" tag = "button" @click.native="">Back</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style>
+  .signup, .login {
+    width: 400px;
+    margin: 0 auto;
+    padding: 20px;
+    text-align: center;
+  }
+  .signup input,
+  .login input {
+    width: 100%;
+    padding: 5px;
+    margin-bottom: 20px;
+    border: 1px solid #ddd;
+  }
+</style>
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,13 @@
 import EventsList from './components/EventsList.vue'
 import NewEditEvent from './components/NewEditEvent.vue'
 import EventInfo from './components/EventInfo.vue'
+import Login from './components/Login.vue'
+import Signup from './components/Signup.vue'
 
 export const routes = [
     {path: '/', component: EventsList},
+    {path: '/signup', component: Signup},
+    {path: '/login', component: Login},
     {path: '/new', component: NewEditEvent},
     {path: '/edit/:id', component: NewEditEvent},
     {path: '/info/:id', component: EventInfo}
